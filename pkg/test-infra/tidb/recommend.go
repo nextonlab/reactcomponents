1b6f5e3f96e6f70764b9a9309c091c49b2937da1
nextonlab <111760665+nextonlab@users.noreply.github.com>
2021-10-26 09:47:22 +0000

    minor rev

--- pkg/test-infra/tidb/recommend.go
+++ pkg/test-infra/tidb/recommend.go
@@ -76,4 +76,258 @@ func (t *Recommendation) EnableTiFlash(clusterConfig fixture.TiDBClusterConfig) 
 		}
 		tiflashCfg := v1alpha1.NewTiFlashConfig()
 		tiflashCfg.Common.Set("logger.level", "debug")
-		t.TidbCluster.Spec.TiFlash
+		t.TidbCluster.Spec.TiFlash.Config = tiflashCfg
+	}
+}
+
+// PDReplicas ...
+func (t *Recommendation) PDReplicas(replicas int32) *Recommendation {
+	t.TidbCluster.Spec.PD.Replicas = replicas
+	return t
+}
+
+// TiKVReplicas ...
+func (t *Recommendation) TiKVReplicas(replicas int32) *Recommendation {
+	t.TidbCluster.Spec.TiKV.Replicas = replicas
+	return t
+}
+
+// TiDBReplicas ...
+func (t *Recommendation) TiDBReplicas(replicas int32) *Recommendation {
+	t.TidbCluster.Spec.TiDB.Replicas = replicas
+	return t
+}
+
+// TiDBReplicas ...
+func (t *Recommendation) defineLogStorageClassName(clusterConfig fixture.TiDBClusterConfig) {
+	logSC := ""
+	if len(fixture.Context.LocalVolumeStorageClass) > 0 {
+		logSC = fixture.Context.LocalVolumeStorageClass
+	}
+	if len(clusterConfig.LogStorageClassName) > 0 {
+		logSC = clusterConfig.LogStorageClassName
+	}
+	t.TidbCluster.Spec.PD.StorageVolumes[0].StorageClassName = &logSC
+	t.TidbCluster.Spec.TiDB.StorageVolumes[0].StorageClassName = &logSC
+}
+
+func providePDStorageClassName(clusterConfig fixture.TiDBClusterConfig) string {
+	sc := fixture.Context.LocalVolumeStorageClass
+	if len(fixture.Context.TiDBClusterConfig.PDStorageClassName) > 0 {
+		sc = fixture.Context.TiDBClusterConfig.PDStorageClassName
+	}
+	if len(clusterConfig.PDStorageClassName) > 0 {
+		sc = clusterConfig.PDStorageClassName
+	}
+	return sc
+}
+
+func provideTiKVStorageClassName(clusterConfig fixture.TiDBClusterConfig) string {
+	sc := fixture.Context.LocalVolumeStorageClass
+	if len(fixture.Context.TiDBClusterConfig.TiKVStorageClassName) > 0 {
+		sc = fixture.Context.TiDBClusterConfig.TiKVStorageClassName
+	}
+	if len(clusterConfig.TiKVStorageClassName) > 0 {
+		sc = clusterConfig.TiKVStorageClassName
+	}
+	return sc
+}
+
+func provideTiFlashStorageClassName(clusterConfig fixture.TiDBClusterConfig) string {
+	sc := fixture.Context.LocalVolumeStorageClass
+	if len(fixture.Context.TiDBClusterConfig.TiFlashStorageClassName) > 0 {
+		sc = fixture.Context.TiDBClusterConfig.TiFlashStorageClassName
+	}
+	if len(clusterConfig.TiFlashStorageClassName) > 0 {
+		sc = clusterConfig.TiFlashStorageClassName
+	}
+	return sc
+}
+
+// RecommendedTiDBCluster does a recommendation, tidb-operator do not have same defaults yet
+func RecommendedTiDBCluster(ns, name string, clusterConfig fixture.TiDBClusterConfig) *Recommendation {
+	enablePVReclaim, exposeStatus := true, true
+	reclaimDelete := corev1.PersistentVolumeReclaimDelete
+	pdDataStorageClass := providePDStorageClassName(clusterConfig)
+	tikvDataStorageClass := provideTiKVStorageClassName(clusterConfig)
+
+	pdBaseImage, pdVersion := util.BuildBaseImageAndVersion("pd", clusterConfig.ImageVersion, clusterConfig.PDImage)
+	tikvBaseImage, tikvVersion := util.BuildBaseImageAndVersion("tikv", clusterConfig.ImageVersion, clusterConfig.TiKVImage)
+	tidbBaseImage, tidbVersion := util.BuildBaseImageAndVersion("tidb", clusterConfig.ImageVersion, clusterConfig.TiDBImage)
+	ticdcBaseImage, ticdcVersion := util.BuildBaseImageAndVersion("ticdc", clusterConfig.ImageVersion, clusterConfig.TiCDCImage)
+	version := tidbVersion
+
+	r := &Recommendation{
+		TidbCluster: &v1alpha1.TidbCluster{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      name,
+				Namespace: ns,
+				Labels: map[string]string{
+					"app":      "tipocket-tidbcluster",
+					"instance": name,
+				},
+			},
+			Spec: v1alpha1.TidbClusterSpec{
+				Timezone:        "UTC",
+				PVReclaimPolicy: &reclaimDelete,
+				EnablePVReclaim: &enablePVReclaim,
+				ImagePullPolicy: corev1.PullAlways,
+				Version:         version,
+				PD: &v1alpha1.PDSpec{
+					Replicas:             int32(clusterConfig.PDReplicas),
+					ResourceRequirements: fixture.WithStorage(fixture.Medium, "10Gi"),
+					StorageClassName:     &pdDataStorageClass,
+					BaseImage:            pdBaseImage,
+					ComponentSpec: v1alpha1.ComponentSpec{
+						Version: &pdVersion,
+					},
+					StorageVolumes: []v1alpha1.StorageVolume{
+						{
+							Name:        "log",
+							StorageSize: "200Gi",
+							MountPath:   "/var/log/pdlog",
+						},
+					},
+				},
+				TiKV: &v1alpha1.TiKVSpec{
+					Replicas: int32(clusterConfig.TiKVReplicas),
+					ResourceRequirements: fixture.WithStorage(corev1.ResourceRequirements{
+						Requests: corev1.ResourceList{
+							fixture.CPU:    resource.MustParse("500m"),
+							fixture.Memory: resource.MustParse("1Gi"),
+						},
+						Limits: corev1.ResourceList{
+							fixture.CPU:    resource.MustParse("1000m"),
+							fixture.Memory: resource.MustParse("16Gi"),
+						},
+					}, "200Gi"),
+					StorageClassName: &tikvDataStorageClass,
+					// disable auto fail over
+					MaxFailoverCount: pointer.Int32Ptr(int32(0)),
+					BaseImage:        tikvBaseImage,
+					ComponentSpec: v1alpha1.ComponentSpec{
+						Version: &tikvVersion,
+					},
+				},
+				TiDB: &v1alpha1.TiDBSpec{
+					Replicas: int32(clusterConfig.TiDBReplicas),
+					ResourceRequirements: corev1.ResourceRequirements{
+						Requests: corev1.ResourceList{
+							fixture.CPU:    resource.MustParse("500m"),
+							fixture.Memory: resource.MustParse("512Mi"),
+						},
+						Limits: corev1.ResourceList{
+							fixture.CPU:    resource.MustParse("8000m"),
+							fixture.Memory: resource.MustParse("16Gi"),
+						},
+					},
+					Service: &v1alpha1.TiDBServiceSpec{
+						ServiceSpec: v1alpha1.ServiceSpec{
+							Type: corev1.ServiceTypeNodePort,
+						},
+						ExposeStatus: &exposeStatus,
+					},
+					// disable auto fail over
+					MaxFailoverCount: pointer.Int32Ptr(int32(0)),
+					BaseImage:        tidbBaseImage,
+					ComponentSpec: v1alpha1.ComponentSpec{
+						Version: &tidbVersion,
+					},
+					StorageVolumes: []v1alpha1.StorageVolume{
+						{
+							Name:        "log",
+							StorageSize: "200Gi",
+							MountPath:   "/var/log/tidblog",
+						},
+					},
+				},
+				TiCDC: &v1alpha1.TiCDCSpec{
+					Replicas:  int32(clusterConfig.TiCDCReplicas),
+					BaseImage: ticdcBaseImage,
+					ComponentSpec: v1alpha1.ComponentSpec{
+						Version: &ticdcVersion,
+					},
+					ResourceRequirements: corev1.ResourceRequirements{
+						Requests: corev1.ResourceList{
+							fixture.CPU:    resource.MustParse("1000m"),
+							fixture.Memory: resource.MustParse("1Gi"),
+						},
+						Limits: corev1.ResourceList{
+							fixture.CPU:    resource.MustParse("1000m"),
+							fixture.Memory: resource.MustParse("8Gi"),
+						},
+					},
+					Config: &v1alpha1.CDCConfigWraper{
+						GenericConfig: &config.GenericConfig{
+							MP: map[string]interface{}{
+								"timezone": fixture.Context.CDCConfig.Timezone,
+								"logLevel": fixture.Context.CDCConfig.LogLevel,
+								"logFile":  fixture.Context.CDCConfig.LogFile,
+							},
+						},
+					},
+				},
+			},
+		},
+		TidbMonitor: &v1alpha1.TidbMonitor{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      name,
+				Namespace: ns,
+				Labels: map[string]string{
+					"app":      "tipocket-tidbmonitor",
+					"instance": name,
+				},
+			},
+			Spec: v1alpha1.TidbMonitorSpec{
+				Clusters: []v1alpha1.TidbClusterRef{
+					{
+						Namespace: ns,
+						Name:      name,
+					},
+				},
+				Persistent: false,
+				Prometheus: v1alpha1.PrometheusSpec{
+					MonitorContainer: v1alpha1.MonitorContainer{
+						BaseImage: "prom/prometheus",
+						Version:   "v2.11.1",
+					},
+					LogLevel: "info",
+				},
+				Grafana: &v1alpha1.GrafanaSpec{
+					Service: v1alpha1.ServiceSpec{
+						Type: corev1.ServiceType(fixture.Context.TiDBMonitorSvcType),
+					},
+					MonitorContainer: v1alpha1.MonitorContainer{
+						BaseImage: "grafana/grafana",
+						Version:   "6.0.1",
+					},
+				},
+				Initializer: v1alpha1.InitializerSpec{
+					MonitorContainer: v1alpha1.MonitorContainer{
+						BaseImage: "pingcap/tidb-monitor-initializer",
+						Version:   "nightly",
+					},
+				},
+				Reloader: v1alpha1.ReloaderSpec{
+					MonitorContainer: v1alpha1.MonitorContainer{
+						BaseImage: "pingcap/tidb-monitor-reloader",
+						Version:   "v1.0.1",
+					},
+				},
+				ImagePullPolicy: corev1.PullIfNotPresent,
+			},
+		},
+	}
+	if clusterConfig.TiFlashReplicas > 0 {
+		r.EnableTiFlash(clusterConfig)
+	}
+	r.defineLogStorageClassName(clusterConfig)
+	if clusterConfig.Ref != nil {
+		r.TidbCluster.Spec.PDAddresses = []string{
+			fmt.Sprintf("http://%s-pd-0.%s-pd-peer.%s.svc:2379",
+				clusterConfig.Ref.Name, clusterConfig.Ref.Name, clusterConfig.Ref.Namespace),
+		}
+		r.TidbMonitor = nil
+	}
+	return r
+}
---
+++ pkg/test-infra/util/util.go
@@ -0,0 +1,190 @@
+
+// Copyright 2019 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package util
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"html/template"
+	"os"
+	"strings"
+
+	"github.com/ngaut/log"
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/pingcap/tipocket/pkg/cluster"
+	"github.com/pingcap/tipocket/pkg/test-infra/fixture"
+)
+
+// FindPort get possible correct port when there are multiple ports
+func FindPort(podName, component string, containers []corev1.Container) int32 {
+	var container *corev1.Container
+	for idx, c := range containers {
+		if c.Name == component {
+			container = &containers[idx]
+			break
+		}
+	}
+	// if we cannot find the target container according to component name, fallback to the first container
+	if container == nil {
+		log.Errorf("failed to find the main container %s in %s", component, podName)
+		container = &containers[0]
+	}
+	ports := container.Ports
+	var priorityPort int32 = 0
+	if component == string(cluster.PD) {
+		priorityPort = 2379
+	} else if component == string(cluster.TiKV) {
+		priorityPort = 20160
+	} else if component == string(cluster.TiDB) {
+		priorityPort = 4000
+	} else if component == string(cluster.DM) {
+		priorityPort = 8261
+	} else if component == string(cluster.MySQL) {
+		priorityPort = 3306
+	} else if component == string(cluster.TiCDC) {
+		priorityPort = 8301
+	}
+
+	for _, port := range ports {
+		if port.ContainerPort == priorityPort {
+			return priorityPort
+		}
+	}
+
+	return ports[0].ContainerPort
+}
+
+// RenderTemplateFunc ...
+func RenderTemplateFunc(tpl *template.Template, model interface{}) (string, error) {
+	buff := new(bytes.Buffer)
+	err := tpl.Execute(buff, model)
+	if err != nil {
+		return "", err
+	}
+	return buff.String(), nil
+}
+
+// ApplyObject applies k8s object
+func ApplyObject(client client.Client, object runtime.Object) error {
+	if err := client.Create(context.TODO(), object); err != nil {
+		if !apierrors.IsAlreadyExists(err) {
+			return err
+		}
+	}
+	return nil
+}
+
+// BuildImage builds a image URL: ${fixture.Context.HubAddress}/${fixture.Context.DockerRepository}/$name:$tag
+// or returns the fullImageIfNotEmpty if it's not empty
+// Attention: since tidb-operator v1.3.0, image is deprecated actually, so don't use this function to set the tidbCluster CRD component images
+func BuildImage(name, tag, fullImageIfNotEmpty string) string {
+	if len(fullImageIfNotEmpty) > 0 {
+		return fullImageIfNotEmpty
+	}
+	var b strings.Builder
+	hub := chooseHub(name)
+	if hub != "" {
+		fmt.Fprintf(&b, "%s/", hub)
+	}
+	b.WriteString(fixture.Context.DockerRepository)
+	b.WriteString("/")
+	b.WriteString(name)
+	b.WriteString(":")
+	b.WriteString(tag)
+
+	return b.String()
+}
+
+// BuildBaseImageAndVersion generates baseImage and version for tidbCluster CRD components.
+func BuildBaseImageAndVersion(name, tag, fullImageIfNotEmpty string) (baseImage string, version string) {
+	image := BuildImage(name, tag, fullImageIfNotEmpty)
+	s := strings.Split(image, ":")
+	if len(s) == 1 {
+		return image, "latest"
+	}
+	return s[0], s[1]
+}
+
+func chooseHub(image string) string {
+	switch image {
+	case "tidb":
+		if fixture.Context.TiDBClusterConfig.TiDBHubAddress != "" {
+			return fixture.Context.TiDBClusterConfig.TiDBHubAddress
+		}
+	case "tikv":
+		if fixture.Context.TiDBClusterConfig.TiKVHubAddress != "" {
+			return fixture.Context.TiDBClusterConfig.TiKVHubAddress
+		}
+	case "pd":
+		if fixture.Context.TiDBClusterConfig.PDHubAddress != "" {
+			return fixture.Context.TiDBClusterConfig.PDHubAddress
+		}
+	case "tiflash":
+		if fixture.Context.TiDBClusterConfig.TiFlashHubAddress != "" {
+			return fixture.Context.TiDBClusterConfig.TiFlashHubAddress
+		}
+	}
+	return fixture.Context.HubAddress
+}
+
+// GetNodeIPsFromPod gets the IPs (or addresses) for nodes.
+func GetNodeIPsFromPod(cli client.Client, namespace string, podLabels map[string]string) ([]string, error) {
+	var ips []string
+	pods := &corev1.PodList{}
+	if err := cli.List(context.Background(), pods, client.InNamespace(namespace), client.MatchingLabels(podLabels)); err != nil {
+		return ips, err
+	}
+
+	for _, item := range pods.Items {
+		ips = append(ips, item.Status.HostIP)
+	}
+	return ips, nil
+}
+
+// GetServiceByMeta gets the service by its meta.
+func GetServiceByMeta(cli client.Client, svc *corev1.Service) (*corev1.Service, error) {
+	clone := svc.DeepCopy()
+	key, err := client.ObjectKeyFromObject(clone)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := cli.Get(context.Background(), key, clone); err != nil {
+		return nil, err
+	}
+	return clone, nil
+}
+
+// IsInK8sPodEnvironment checks whether in the k8s pod environment
+// refer: https://stackoverflow.com/questions/36639062/how-do-i-tell-if-my-container-is-running-inside-a-kubernetes-cluster/54130803#54130803
+func IsInK8sPodEnvironment() bool {
+	return os.Getenv("KUBERNETES_SERVICE_HOST") != ""
+}
+
+// GetFQDNFromStsPod generates the full qualified domain name from a pod of sts which has a headless(or other service kinds) service with it
+func GetFQDNFromStsPod(pod *corev1.Pod) (string, error) {
+	if pod.Spec.Hostname == "" {
+		return "", fmt.Errorf("expect non-empty .spec.hostname of %s/%s", pod.ObjectMeta.Namespace, pod.ObjectMeta.Name)
+	}
+	if pod.Spec.Subdomain == "" {
+		return "", fmt.Errorf("expect non-empty .spec.subdomain of %s/%s", pod.ObjectMeta.Namespace, pod.ObjectMeta.Name)
+	}
+	return fmt.Sprintf("%s.%s.%s.svc", pod.Spec.Hostname, pod.Spec.Subdomain, pod.Namespace), nil
+}
