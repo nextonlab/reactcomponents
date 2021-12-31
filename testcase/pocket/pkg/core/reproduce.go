3d62c9978bca6efdcde6e933cb71c6657ec55515
nextonlab <111760665+nextonlab@users.noreply.github.com>
2021-12-31 14:40:35 +0000

    fix links in readme

--- testcase/pocket/pkg/core/reproduce.go
+++ testcase/pocket/pkg/core/reproduce.go
@@ -77,3 +77,140 @@ func (c *Core) reproduceFromDir(dir, table string) error {
 	if err != nil {
 		return errors.Trace(err)
 	}
+
+	log.Info(len(logs), "logs")
+	for index, l := range logs {
+		// if index < len(logs) - 1 && logs[index].GetTime() == logs[index + 1].GetTime() {
+		// 	log.Info(logs[index].GetNode(), logs[index].GetSQL())
+		// 	log.Info(logs[index + 1].GetNode(), logs[index + 1].GetSQL())
+		// 	log.Fatal("time mess")
+		// }
+		if index%100 == 0 {
+			log.Info(index, "/", len(logs))
+		}
+		c.executeByID(l.GetNode(), l.GetSQL())
+		// if rand.Float64() < 0.1 {
+		// 	ch := make(chan struct{}, 1)
+		// 	go e.abTestCompareDataWithoutCommit(ch)
+		// 	<- ch
+		// }
+	}
+	log.Info("final check")
+	// e.abTestCompareData(false)
+	result, err := c.checkConsistency(false)
+	if err == nil {
+		log.Infof("consistency check %t\n", result)
+	}
+	return nil
+}
+
+func (c *Core) readLogs(logFiles []string) ([]*types.Log, error) {
+	var serilizedLogs []*types.Log
+	for _, file := range logFiles {
+		logs, err := readLogFile(file)
+		if err != nil {
+			return serilizedLogs, errors.Trace(err)
+		}
+		serilizedLogs = append(serilizedLogs, logs...)
+	}
+	sort.Sort(types.ByLog(serilizedLogs))
+	return serilizedLogs, nil
+}
+
+func readLogFile(logFile string) ([]*types.Log, error) {
+	var (
+		execID = parseExecNumber(logFile)
+		logs   []*types.Log
+	)
+	f, err := os.Open(logFile)
+	if err != nil {
+		log.Fatalf("error when open file %v\n", err)
+	}
+	defer func() {
+		if err := f.Close(); err != nil {
+			log.Errorf("error when close file %v", err)
+		}
+	}()
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := scanner.Text()
+		log, err := parseLog(line, execID)
+		if err == nil {
+			logs = append(logs, log)
+		}
+	}
+	return logs, nil
+}
+
+func parseExecNumber(filePath string) int {
+	m := execIDPattern.FindStringSubmatch(filePath)
+	if len(m) != 3 {
+		return 0
+	}
+	id, err := strconv.Atoi(m[2])
+	if err != nil {
+		return 0
+	}
+	return id
+}
+
+func parseLog(line string, node int) (*types.Log, error) {
+	var (
+		m   []string
+		log types.Log
+	)
+
+	m = todoSQLPattern.FindStringSubmatch(line)
+	if len(m) != 4 {
+		return nil, errors.NotFoundf("not matched line %s", line)
+	}
+
+	t, err := time.Parse(timeLayout, m[1])
+	if err != nil {
+		return nil, err
+	}
+	log.Time = t
+	log.SQL = &types.SQL{
+		SQLType: parseSQLType(m[3]),
+		SQLStmt: m[3],
+	}
+	log.State = m[2]
+
+	log.Node = node
+	return &log, nil
+}
+
+func parseSQLType(sql string) types.SQLType {
+	sql = strings.ToLower(sql)
+	if strings.HasPrefix(sql, "select") {
+		return types.SQLTypeDMLSelect
+	}
+	if strings.HasPrefix(sql, "update") {
+		return types.SQLTypeDMLUpdate
+	}
+	if strings.HasPrefix(sql, "insert") {
+		return types.SQLTypeDMLInsert
+	}
+	if strings.HasPrefix(sql, "delete") {
+		return types.SQLTypeDMLDelete
+	}
+	if strings.HasPrefix(sql, "create table") {
+		return types.SQLTypeDDLCreateTable
+	}
+	if strings.HasPrefix(sql, "alter table") {
+		return types.SQLTypeDDLAlterTable
+	}
+	if strings.HasPrefix(sql, "create index") {
+		return types.SQLTypeDDLCreateIndex
+	}
+	if strings.HasPrefix(sql, "begin") {
+		return types.SQLTypeTxnBegin
+	}
+	if strings.HasPrefix(sql, "commit") {
+		return types.SQLTypeTxnCommit
+	}
+	if strings.HasPrefix(sql, "rollback") {
+		return types.SQLTypeTxnRollback
+	}
+	return types.SQLTypeUnknown
+}
